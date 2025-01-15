63d606434138c49a6cac50e3d2a6e691c0fd4b40
simon <sirnomidea@gmail.com>
2025-01-15 21:27:33 +1300

    update build

--- vite.config.ts
+++ vite.config.ts
@@ -18,4 +18,15 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].[hash].js`,
+        chunkFileNames: `assets/[name].[hash].js`,
+        assetFileNames: `assets/[name].[hash].[ext]`
+      }
+    }
+  }
 })
