628aea36766afc0bcdaba8690d6f549a3c7fd535
simon <sirnomidea@gmail.com>
2025-01-15 21:53:42 +1300

    fix base path

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
     vueJsx(),
     vueDevTools(),
   ],
-  base: './',
+  base: '/mcs/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
